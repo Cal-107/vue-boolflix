59fcd16355aa3c3a360683f532d4e81b24aa8bba
“Cal-107” <kaly___7@hotmail.it>
2021-11-29 18:40:12 +0100

    add flags

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,7 +6,10 @@
        <li>
            {{ subTitle }}
        </li>
-       <li>
+       <li v-if="languagesFlags.includes(text)">
+           <img :src="require(`../assets/${text}.png`)" alt="">
+       </li>
+       <li v-else>
            {{ text }}
        </li>
        <li>
@@ -18,14 +21,24 @@
 <script>
 export default {
     name: 'Card',
+    data() {
+        return {
+            languagesFlags: ['it', 'en'],
+        }
+    },
     props: {
         title: String,
         subTitle: String,
         text: String,
         number: Number,
-    }
+    },
 }
 </script>
 
 <style lang="scss" scoped>
+li {
+    img {
+        width: 30px;
+    }
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -23,7 +23,7 @@ export default {
     },
     props: {
         propArray: Array,
-    }
+    },
 }
 </script>
 
