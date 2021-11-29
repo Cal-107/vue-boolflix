1946d55a7cc43d699d69c5c57e1f6392e955fc5d
“Cal-107” <kaly___7@hotmail.it>
2021-11-29 12:56:43 +0100

    add check for API

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,42 @@
 <template>
   <main>
       <ul>
-          <li></li>
+          <li v-for="(movie, i) in movieList" :key="`movie-${i}`">
+              {{ movie.original_title}}
+          </li>
       </ul>
   </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'Main',
+    data() {
+        return {
+            movieList: null,
+        }
+    },
+    created() {
+        this.getMovieList()
+    },
+    methods: {
+        getMovieList() {
+            axios
+            .get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: '7872b69ec498a7e0e4e9f9dadbe23059',
+                    query: 'jurassic park',
+                    language: 'it-IT',
+                },
+            })
+            .then(response => {
+                this.movieList = response.data.results;
+            })
+            .catch(err => console.log(err));
+        },
+    }
 }
 </script>
 
