d840c5952dca66c4e1f42d1801b08be6af86fdaf
“Cal-107” <kaly___7@hotmail.it>
2021-11-29 14:16:10 +0100

    add axios on app.vue, prop array for card

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,48 @@
   <div id="app">
     <Header />
 
-    <Main />
+    <Main :propArray="movieList" />
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Header from './components/Header.vue';
 import Main from './components/Main.vue';
 
 export default {
   name: 'App',
-  components: {
-    Header,
-    Main,
+    components: {
+      Header,
+      Main,
+    },
+
+  data() {
+      return {
+          movieList: null,
+      }
+  },
+
+
+  created() {
+      this.getMovieList()
+  },
+
+  methods: {
+      getMovieList() {
+          axios
+          .get('https://api.themoviedb.org/3/search/movie', {
+              params: {
+                  api_key: '7872b69ec498a7e0e4e9f9dadbe23059',
+                  query: 'matrix',
+                  language: 'it-IT',
+              },
+          })
+          .then(response => {
+              this.movieList = response.data.results;
+          })
+          .catch(err => console.log(err));
+      },
   }
 }
 </script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,31 +1,18 @@
 <template>
   <main>
-    <ul v-for="(movie, i) in movieList" :key="`movie-${i}`">
+    <ul v-for="(movie, i) in propArray" :key="`movie-${i}`">
         <Card 
             :title="movie.title"
             :subTitle="movie.original_title"
             :text="movie.original_language"
             :number="movie.vote_average"
         />
-
-       <!-- <li>
-           {{ movie.title }}
-       </li>
-       <li>
-           {{ movie.original_title }}
-       </li>
-       <li>
-           {{ movie.original_language }}
-       </li>
-       <li>
-           {{ movie.vote_average }}
-       </li> -->
     </ul>
   </main>
 </template>
 
 <script>
-import axios from 'axios';
+// import axios from 'axios';
 import Card from '@/components/Card';
 
 export default {
@@ -34,32 +21,8 @@ export default {
     components: {
         Card,
     },
-
-    data() {
-        return {
-            movieList: null,
-        }
-    },
-
-    created() {
-        this.getMovieList()
-    },
-
-    methods: {
-        getMovieList() {
-            axios
-            .get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: '7872b69ec498a7e0e4e9f9dadbe23059',
-                    query: 'matrix',
-                    language: 'it-IT',
-                },
-            })
-            .then(response => {
-                this.movieList = response.data.results;
-            })
-            .catch(err => console.log(err));
-        },
+    props: {
+        propArray: Array,
     }
 }
 </script>
