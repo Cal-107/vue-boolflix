5e716554423bebc91ea459dbfeb6099911f03f2a
“Cal-107” <kaly___7@hotmail.it>
2021-11-29 11:53:30 +0100

    add setup

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+      <div>
+
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+      <input type="text" placeholder="Cosa vuoi cercare?">
+      <button class="searchBtn">Search</button>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    padding: 1rem;
+    button {
+        margin-left: 1rem;
+    }
+}
+</style>
