31d76405f820c98d3368bcc9ee2ed2f4356a1933
“Cal-107” <kaly___7@hotmail.it>
2021-11-30 18:29:32 +0100

    add columns

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,40 @@
 <template>
   <main>
-    <ul v-for="movie in propArray" :key="movie.id">
-        <Card 
-            :image="movie.poster_path"
-            :title="movie.title"
-            :subTitle="movie.original_title"
-            :text="movie.original_language"
-            :number="movie.vote_average"
-        />
-    </ul>
-
-    <ul v-for="serie in propArray2" :key="serie.id">
-        <Card 
-            :image="serie.poster_path"
-            :title="serie.name"
-            :subTitle="serie.original_name"
-            :text="serie.original_language"
-            :number="serie.vote_average"
-        />
-    </ul>
+    <section class="my-box">
+      <div>
+          <h1 v-show="propArray2 != 0"> Movies List</h1>
+          <ul 
+                class="movies" 
+                v-for="movie in propArray" 
+                :key="movie.id"
+            >
+              <Card 
+                  :image="movie.poster_path"
+                  :title="movie.title"
+                  :subTitle="movie.original_title"
+                  :text="movie.original_language"
+                  :number="movie.vote_average"
+              />
+          </ul>
+      </div>
+    
+      <div>
+        <h1 v-show="propArray2 != 0"> Series List </h1>
+        <ul 
+            class="series" 
+            v-for="serie in propArray2" 
+            :key="serie.id"
+        >
+            <Card 
+                :image="serie.poster_path"
+                :title="serie.name"
+                :subTitle="serie.original_name"
+                :text="serie.original_language"
+                :number="serie.vote_average"
+            />
+        </ul>
+      </div>
+    </section>  
   </main>
 </template>
 
@@ -40,4 +56,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.my-box {
+    h1 {
+        text-align: center;
+    }
+    display: flex;
+    .movies {
+        section {
+            display: flex;
+            flex-direction: column;
+            img {
+                align-self: center;
+            }
+        }
+    }
+    .series {
+        section {
+            display: flex;
+            flex-direction: column;
+        }
+    }
+}
 </style>
